6e231206f4f81e8d7abff9a9c8167a395a5f6df7
Gabriel Oliveira <gabrieloliveira851@hotmail.com>
2023-08-15 23:09:07 -0300

    Initial Commit

---
+++ apiaviacaosystem/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace apiaviacaosystem
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
